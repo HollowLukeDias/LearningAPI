801a8c9b5e293331cc0443afd60fa096ae7f7805
LukeDias <lhollowwizard@gmail.com>
2020-12-02 21:10:35 -0300

    Created a very simple API and relearned HTTP Verbs

---
+++ LearningAPI/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningAPI.Models;
+
+namespace LearningAPI.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        static List<Product> _products = new List<Product>()
+        {
+            new Product(){ProductID = 0, ProductName = "Death", ProductPrice = "200000"},
+            new Product(){ProductID = 0, ProductName = "Life", ProductPrice = "0"}
+        };
+
+        public IEnumerable<Product> Get()
+        {
+            return _products;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Product product)
+        {
+            _products.Add(product);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Product product)
+        {
+            _products[id] = product;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _products.RemoveAt(id);
+        }
+
+    }
+}
